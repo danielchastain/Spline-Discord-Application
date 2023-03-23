433ed80b1f7c2b259cec13afcb271afa38535195
Daniel Chastain <53002994+danielchastain@users.noreply.github.com>
2023-03-23 11:20:23 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Spline from '@splinetool/react-spline';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+
+    <>
+      <body>
+        <div className="container-fluid">
+          <Spline scene="https://prod.spline.design/QabuomaV6XmEDysB/scene.splinecode" />
+        </div>
+      </body>
+
+
+    </>
+
+  );
+}
