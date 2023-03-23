5dcac51217a4979cc2b3405fcb25d67a73d9339a
Daniel Chastain <53002994+danielchastain@users.noreply.github.com>
2023-03-23 11:49:10 +0000

    bigger frame

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
-
     <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
       <body>
-        <div className="container-fluid">
-          <Spline scene="https://prod.spline.design/QabuomaV6XmEDysB/scene.splinecode" />
+        <div className="bg-body-tertiary">
+          <div className="tp-7">
+            <Spline scene="https://prod.spline.design/QabuomaV6XmEDysB/scene.splinecode" />
+          </div>
+
+        <div>Website</div>
         </div>
       </body>
 
